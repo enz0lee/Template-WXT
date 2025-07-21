8a4320c55e64f73fb89783f2635d28f7bad58eac
Enzo Lee <soloenzo.dev@gmail.com>
2025-07-21 13:38:54 +0700

    feat: add jsx-a11y plugin and update ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
@@ -10,19 +11,14 @@ export default [
   autoImports,
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       react,
       'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
     },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-    },
-  },
-  eslintConfigPrettier,
-  {
     languageOptions: {
       parser: tseslint.parser,
       ecmaVersion: 'latest',
@@ -44,7 +40,10 @@ export default [
       },
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/alt-text': 'error',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
